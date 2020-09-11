896305b930046217fb3ab314bf9af5c6c8185a68
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-09-11 00:58:00 -0400

    got it to print a welcome message that will be the start of my game.

--- src/adv.py
+++ src/adv.py
@@ -1,37 +1,14 @@
-from room import Room
+# from room import Room
+# from player import player
 
 # Declare all the rooms
 
-room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
-}
 
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+
 
 #
 # Main
@@ -49,3 +26,19 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+def show_welcome_message():
+    welcome_message = "Welcome one and all to a game of Blackout Uno!"
+    print(welcome_message)
+
+def quit_game():
+    quit_message = "Thank you for not setting things on fire, please play again!"
+    if cmd == "q":
+        print(quit_message)
+
+welcome_message = "Welcome one and all to a game of Blackout Uno!"
+quit_message = "Thank you for not setting things on fire, please play again!"
+win_message = "You Won!"
+loss_message = "Sorry, but you lost this time."
+
+show_welcome_message()
