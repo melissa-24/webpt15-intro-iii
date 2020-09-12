3e50ec30dd7a09a3a8e0285fa5806489bd8b8235
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-09-11 23:13:04 -0400

    got the rooms layed out and linked but it throws errors.  Able to get the game started though

--- src/adv.py
+++ src/adv.py
@@ -1,12 +1,39 @@
 # from room import Room
-# from player import player
+# # from player import player
 
-# Declare all the rooms
+# # Declare all the rooms
+# room = {
+#     'town-square': Room('town-square', 'Gathering for all who want to help the Gods'),
+#     'ternion': Room('ternion', 'Where the big 3, Zeus, Poseidon, and Hades reside'),
+#     'ancillary': Room('ancillary', 'Where the rest of the God reside'),
+#     'eminence': Room('eminence', 'The residence of the Demi-Gods'),
+#     'olympus': Room('olympus', 'Zeus domain'),
+#     'ocean': Room('ocean','Poseidon domain'),
+#     'abyss': Room('abyss','Hades domain'),
+#     'camp': Room('camp', 'Training area'),
+# }
 
 
+# # Link rooms together
 
-
-# Link rooms together
+# room['olympus'].n_to = room['ternion']
+# room['ternion'].s_to = room['olympus']
+# room['ternion'].n_to = room['town-square']
+# room['town-square'].s_to = room['ternion']
+# room['ocean'].w_to = room['ternion']
+# room['ternion'].e_to = room['ocean']
+# room['ternion'].w_to = room['abyss']
+# room['abyss'].e_to = room['ternion']
+# room['ocean'].n_to = room['ancillary']
+# room['ancillary'].s_to = room['ocean']
+# room['ancillary'].w_to = room['town-square']
+# room['town-square'].e_to = room['ancillary']
+# room['town-square'].w_to = room['eminence']
+# room['eminence'].e_to = room['town-square']
+# room['abyss'].n_to = room['eminence']
+# room['eminence'].s_to = room['abyss']
+# room['eminence'].n_to = room['camp']
+# room['camp'].s_to = room['eminence']
 
 
 
@@ -27,18 +54,20 @@
 #
 # If the user enters "q", quit the game.
 
-def show_welcome_message():
-    welcome_message = "Welcome one and all to a game of Blackout Uno!"
-    print(welcome_message)
+answer = input('Welcome to Mythical Assist Adventures.  Would you like to play? (yes/no) ')
 
-def quit_game():
-    quit_message = "Thank you for not setting things on fire, please play again!"
-    if cmd == "q":
-        print(quit_message)
+if answer.lower().strip() == 'yes':
+    name = input('What is your name kind helper of the Gods?').strip()
+    print(f'\nWelcome {name}, lets get you to the camp')
+    items = input('Before we tell you who needs help would you like to pick up some gear?')
+    if items.lower().strip() == 'yes':
+        item = input('Wonderful idea, we have swords and shields available which do you chose?')
+        print(f'\nA {item} is a valiant choice {name}')
 
-welcome_message = "Welcome one and all to a game of Blackout Uno!"
-quit_message = "Thank you for not setting things on fire, please play again!"
-win_message = "You Won!"
-loss_message = "Sorry, but you lost this time."
 
-show_welcome_message()
+    else:
+        print('Maybe you need some more time to prepare. Please come back later')
+
+
+else:
+    print('Maybe next time!')
